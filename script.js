bde49dcb3c9aa4dd5c30ce85b0facc9387d1df42
D_Zucho <danny.nico.dev@outlook.com>
2021-06-05 13:01:23 -0700

    add audio and style transition

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /**
- * TODO: Create a keydown listener to track what keys are hit
- * TODO: Create a Beat class to represent the beat object in utils.js
- * TODO: Complete triggerBeat() to play upon the press of a,s,d,f,g,h,j,k,l
- * TODO: Button pt1: Initialize color and element values
- * TODO: Button pt2: Set button color upon initialization | Initialize button in beats["65"]
- * TODO: Button pt3: Complete select function to set the color and shadow of button upon pressing
+ * DONE: Create a keydown listener to track what keys are hit
+ * DONE: Create a Beat class to represent the beat object in utils.js
+ * DONE: Complete triggerBeat() to play upon the press of a,s,d,f,g,h,j,k,l
+ * DONE: Button pt1: Initialize color and element values
+ * DONE: Button pt2: Set button color upon initialization | Initialize button in beats["65"]
+ * DONE: Button pt3: Complete select function to set the color and shadow of button upon pressing
  * TODO: Button pt4: Call the select() function upon key press ;)
  * TODO: Button pt5: Add transition for button selection
  * TODO: Button pt6: Remove the button style upon transition end | Use deselect function
@@ -18,30 +18,39 @@
 let beats = {
   65: {
     beat: new Beat('./assets/Piano Chord 331.mp3'),
+    button: new Button('#00fffe', 65),
   },
   83: {
     beat: new Beat('./assets/Piano Chord 209.mp3'),
+    button: new Button('#00fffe', 83),
   },
   68: {
     beat: new Beat('./assets/Piano Chord 208.mp3'),
+    button: new Button('#00fffe', 68),
   },
   70: {
     beat: new Beat('./assets/Drum Sticks Hit 3.mp3'),
+    button: new Button('#FF00FF', 70),
   },
   71: {
     beat: new Beat('./assets/Drum Snare Roll.mp3'),
+    button: new Button('#FF00FF', 71),
   },
   72: {
     beat: new Beat('./assets/PREL Musical 57.mp3'),
+    button: new Button('#FF00FF', 72),
   },
   74: {
     beat: new Beat('./assets/Cymbal Suspended 2.mp3'),
+    button: new Button('#FFFFFF', 74),
   },
   75: {
     beat: new Beat('./assets/Musical Compos 33.mp3'),
+    button: new Button('#FFFFFF', 75),
   },
   76: {
     beat: new Beat('./assets/Musical Orches 4.mp3'),
+    button: new Button('#FFFFFF', 76),
   },
 }
 
@@ -49,7 +58,15 @@ let beats = {
  * Function to play the beat upon a press of key
  * HINT: use the keyCode
  */
-triggerBeat = (event) => {}
+triggerBeat = (e) => {
+  const keyCode = e.keyCode
+
+  if (keyCode in beats) {
+    let keyPress = beats[keyCode]
+    keyPress.beat.play()
+    beats[e.keyCode].button.select()
+  }
+}
 
 /**
  * Keydown listener to fire triggerBeat function
@@ -57,4 +74,5 @@ triggerBeat = (event) => {}
  */
 document.addEventListener('keydown', (e) => {
   console.log(e.keyCode)
+  triggerBeat(e)
 })
--- utils.js
+++ utils.js
@@ -9,7 +9,7 @@ class Beat {
   }
 
   play = () => {
-    this.audio.currentTime = 0
+    this.audio.currentTime = 0 // so that if you double press, it starts over
     this.audio.play()
   }
 }
@@ -18,20 +18,40 @@ class Beat {
  * Button class that keeps track of the button color based on a press
  */
 class Button {
-  constructor(color, keyCode) {}
+  constructor(color, keyCode) {
+    this.color = color
+    this.keyCode = keyCode
+    this.element = document.getElementById(keyCode)
+    this.setButtonColorInHTML()
+    this.setTransitionEndListener()
+  }
 
+  setTransitionEndListener = () => {
+    this.element.addEventListener('transitionend', () => {
+      console.log('transition ended')
+      this.deselect()
+    })
+  }
   /**
    * Set the button color based on color specified
    */
-  setButtonColorInHTML = () => {}
+  setButtonColorInHTML = () => {
+    this.element.style.borderColor = this.color
+  }
 
   /**
    * Select function to set the background color and boxShadow
    */
-  select = () => {}
+  select = () => {
+    this.element.style.background = this.color
+    this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`
+  }
 
   /**
    * Deselect function to reset background color and boxShadow
    */
-  deselect = () => {}
+  deselect = () => {
+    this.element.style.background = 'transparent'
+    this.element.style.boxShadow = 'none'
+  }
 }
